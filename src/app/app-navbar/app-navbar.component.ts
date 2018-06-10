20dfc7530b6244ec3216f2d9429bbc03aecb811f
Aïman Wakidou <aimanwakidou@gmail.com>
2018-06-10 15:41:50 +0200

    Testing SCSS

--- src/app/app-navbar/app-navbar.component.ts
+++ src/app/app-navbar/app-navbar.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
-  selector: 'app-app-navbar',
+  selector: 'app-navbar',
   templateUrl: './app-navbar.component.html',
   styleUrls: ['./app-navbar.component.scss']
 })
